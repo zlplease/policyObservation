0884b109d08d712957f065ae6a6588cef5178953
les-Paradis <67249386+zlplease@users.noreply.github.com>
2021-11-03 16:49:02 +0800

    [init]初始化项目，注入依赖

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="hello">
+    <div class="test">
+      wzy,yyds
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App'
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style lang="scss" scoped>
+.hello {
+  .test {
+    color: aqua;
+  }
+}
+</style>
