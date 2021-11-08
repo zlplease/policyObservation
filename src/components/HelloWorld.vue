04d7ea86fade8abb230d9b8a9dfe1e623d0f81a2
les-Paradis <67249386+zlplease@users.noreply.github.com>
2021-11-08 16:48:50 +0800

    [update]地图组件封装

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,27 +1,65 @@
 <template>
   <div class="hello">
     <div class="test">
-      wzy,yyds
+      <chineseMap @province_selected="selectProvince" 
+                  @province_hovered="hoverProvince"
+                  @coordinate="coordinate"
+                  @leave="leave"
+                  class="chineseMap">
+      </chineseMap>
+    </div>
+    <div v-if="this.infoShow" class="provinceInfo" :style="{top: infoY + 'px', left: infoX + 'px'}">
+      hhh
     </div>
   </div>
 </template>
 
 <script>
+import chineseMap from '../components/map.vue'
 export default {
   name: 'HelloWorld',
+  components: {
+    chineseMap
+  },
   data () {
     return {
-      msg: 'Welcome to Your Vue.js App'
+      infoShow: false,
+      infoX: 0,
+      infoY: 0
+    }
+  },
+  methods: {
+    selectProvince(data) {
+      // console.log(data)
+    },
+    hoverProvince(data) {
+      this.infoShow = true
+    },
+    coordinate(x, y) {
+      this.infoX = x
+      this.infoY = y
+    },
+    leave() {
+      this.infoShow = false
     }
   }
 }
 </script>
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
 <style lang="scss" scoped>
 .hello {
   .test {
     color: aqua;
+  }
+  .chineseMap {
+    width: 800px;
+  }
+  .provinceInfo {
+    width: 100px;
+    height: 40px;
+    // background-color: aqua;
+    border-radius: 20px;
+    position: absolute;
   }
 }
 </style>
